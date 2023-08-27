ed7a4d7e96cde030edc27113212e8a63a079a276
YRQV <yaldram_saad@bah.com>
2023-08-27 13:18:25 -0400

    get views code updates

--- Terraform/backend/code/get_views.py
+++ Terraform/backend/code/get_views.py
@@ -1,18 +1,29 @@
 import json
+import logging
+import os
+
 import boto3
+from botocore.exceptions import ClientError
+
+LOGLEVEL = os.environ.get('LOGLEVEL', 'INFO').upper()
+logging_format = '[%(levelname)s] %(filename)s:%(lineno)d %(message)s'
+logging.basicConfig(
+    level=LOGLEVEL,
+    format=logging_format,
+)
+
+logging.getLogger('botocore').setLevel(logging.ERROR)
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('crc-counter')
 
 def lambda_handler(event, context):
 
-    response = table.get_item(Key={"CounterID": '1'})
-    views = response['Item']['views']
-    views = views + 1
-    print(views)
-
-    #response = table.put_item(Item={
-    #    'CounterID': '1',
-    #    'views': views
-    #})
+    try:
+        response = table.get_item(Key={"CounterID": '1'})
+        views = response['Item']['views']
+        logging.info(f'Current total view count is ${views}.')
+    except ClientError as e:
+        logging.error(e)
     return views
+
